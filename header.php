c0b1e2a7e8a4bf8b20963e8b83111454db1f1245
eoghanmccarthy <eoghan.mccarthy@LONFWD-DM1743.local>
2016-11-08 08:21:09 +0000

    add header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <header class="col-md-12">
+        <img class="logo" src="images/logo.png">
+        <hr>
+        <ul>
+            <li><a>photography</a></li>
+            <li><a>contact</a></li>
+        </ul>
+    </header>
+</div>
