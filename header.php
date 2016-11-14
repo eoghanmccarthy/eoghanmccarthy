156124bef1e0d357d14da302adfb05d79f9d2c8a
eoghanmccarthy <eoghan.mccarthy@LONFWD-DM1743.local>
2016-11-14 09:26:54 +0000

    blocks

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <div class="row">
     <header class="col-md-12">
-        <img class="header__logo" src="img/logo.png">
+        <a href="index.php"><img class="header__logo" src="img/logo.png"></a>
         <hr>
         <ul>
             <li><a href="photography.php">photography</a></li>
