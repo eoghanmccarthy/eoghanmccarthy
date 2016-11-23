f5040dc213399f94d41340470a0e307cda272b78
eoghanmccarthy <eoghan.mccarthy@LONFWD-DM1743.local>
2016-11-23 09:16:08 +0000

    jquery link

--- index.php
+++ index.php
@@ -17,6 +17,9 @@
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js" integrity="VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
 
+        <!-- Latest compiled and minified JQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
         <!-- Font Awesome -->
         <script src="https://use.fontawesome.com/e6985d2f93.js"></script>
 
