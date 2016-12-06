381941a4a8295a72de45823984be3bd48a4bc64c
eoghanmccarthy <eoghan.mccarthy@LONFWD-DM1743.local>
2016-12-06 09:39:44 +0000

    high light

--- contact.php
+++ contact.php
@@ -24,7 +24,7 @@
 
             <main class="main">
                 <div class="contact">
-                    <p class="contact__meta"><mark>mail@eoghanmccarthy.com</mark><br><mark>+44 (0)7490 816 795</mark></p>
+                    <p class="contact__meta"><mark class="hlight">mail@eoghanmccarthy.com</mark><br><mark class="hlight">+44 (0)7490 816 795</mark></p>
                 </div>
             </main>
 
