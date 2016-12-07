9d868407fd9f06eeb1347b0c446d3a766977bd48
eoghanmccarthy <eoghan.mccarthy@LONFWD-DM1743.local>
2016-12-07 13:44:40 +0000

    fix

--- partials/lightbox-photo.php
+++ partials/lightbox-photo.php
@@ -5,35 +5,35 @@
   <div class="lightbox__viewer">
 
     <div class="lightbox__slide">
-      <img src="photography/img_0111-folio.jpg" style="width:100%">
+      <img src="photography/img_0111-folio.jpg">
     </div>
 
     <div class="lightbox__slide">
-      <img src="photography/img_0243-folio.jpg" style="width:100%">
+      <img src="photography/img_0243-folio.jpg">
     </div>
 
     <div class="lightbox__slide">
-      <img src="photography/img_0359-folio.jpg" style="width:100%">
+      <img src="photography/img_0359-folio.jpg">
     </div>
 
     <div class="lightbox__slide">
-      <img src="photography/img_0387-folio.jpg" style="width:100%">
+      <img src="photography/img_0387-folio.jpg">
     </div>
 
     <div class="lightbox__slide">
-      <img src="photography/img_4453-folio.jpg" style="width:100%">
+      <img src="photography/img_4453-folio.jpg">
     </div>
 
     <div class="lightbox__slide">
-      <img src="photography/img_4480-folio.jpg" style="width:100%">
+      <img src="photography/img_4480-folio.jpg">
     </div>
 
     <div class="lightbox__slide">
-      <img src="photography/img_3889-folio.jpg" style="width:100%">
+      <img src="photography/img_3889-folio.jpg">
     </div>
 
     <div class="lightbox__slide">
-      <img src="photography/img_3251-folio.jpg" style="width:100%">
+      <img src="photography/img_3251-folio.jpg">
     </div>
 
     <a class="lightbox__prev" onclick="plusSlides(-1)">&#10094;</a>
