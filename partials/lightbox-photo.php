a584cd87b68f957fe020b06f8f88cf44280b9113
eoghanmccarthy <eoghan.mccarthy@LONFWD-DM1743.local>
2016-12-13 08:59:57 +0000

    lightbox fix

--- partials/lightbox-photo.php
+++ partials/lightbox-photo.php
@@ -1,44 +1,44 @@
 <div id="photoLightbox" class="lightbox">
 
-  <span class="lightbox__close" onclick="closeModal()">&times;</span>
+    <span class="lightbox__close" onclick="closeModal()">&times;</span>
 
-  <div class="lightbox__viewer">
+    <div class="lightbox__viewer">
 
-    <div class="lightbox__slide">
-      <img src="photography/img_0111-folio.jpg">
+        <div class="lightbox__slide">
+            <img src="photography/img_0111-folio.jpg">
+        </div>
+
+        <div class="lightbox__slide">
+            <img src="photography/img_0243-folio.jpg">
+        </div>
+
+        <div class="lightbox__slide">
+            <img src="photography/img_0359-folio.jpg">
+        </div>
+
+        <div class="lightbox__slide">
+            <img src="photography/img_0387-folio.jpg">
+        </div>
+
+        <div class="lightbox__slide">
+            <img src="photography/img_4453-folio.jpg">
+        </div>
+
+        <div class="lightbox__slide">
+            <img src="photography/img_4480-folio.jpg">
+        </div>
+
+        <div class="lightbox__slide">
+            <img src="photography/img_3889-folio.jpg">
+        </div>
+
+        <div class="lightbox__slide">
+            <img src="photography/img_3251-folio.jpg">
+        </div>
+
+        <a class="lightbox__viewer--prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="lightbox__viewer--next" onclick="plusSlides(1)">&#10095;</a>
+
     </div>
-
-    <div class="lightbox__slide">
-      <img src="photography/img_0243-folio.jpg">
-    </div>
-
-    <div class="lightbox__slide">
-      <img src="photography/img_0359-folio.jpg">
-    </div>
-
-    <div class="lightbox__slide">
-      <img src="photography/img_0387-folio.jpg">
-    </div>
-
-    <div class="lightbox__slide">
-      <img src="photography/img_4453-folio.jpg">
-    </div>
-
-    <div class="lightbox__slide">
-      <img src="photography/img_4480-folio.jpg">
-    </div>
-
-    <div class="lightbox__slide">
-      <img src="photography/img_3889-folio.jpg">
-    </div>
-
-    <div class="lightbox__slide">
-      <img src="photography/img_3251-folio.jpg">
-    </div>
-
-    <a class="lightbox__prev" onclick="plusSlides(-1)">&#10094;</a>
-    <a class="lightbox__next" onclick="plusSlides(1)">&#10095;</a>
-
-  </div>
 
 </div>
