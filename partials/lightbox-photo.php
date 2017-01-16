d0d701721947abf58bf779a55db0b7b33198ac1a
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-01-16 23:34:12 +0000

    lightbox

--- partials/lightbox-photo.php
+++ partials/lightbox-photo.php
@@ -1,44 +1,23 @@
 <div id="photoLightbox" class="lightbox">
-
     <span class="lightbox__close" onclick="closeModal()">&times;</span>
-
     <div class="lightbox__viewer">
-
-        <div class="lightbox__slide">
-            <img src="photography/img_0111-folio.jpg">
+        <div class="lightbox__slide slide-01">
         </div>
-
-        <div class="lightbox__slide">
-            <img src="photography/img_0243-folio.jpg">
+        <div class="lightbox__slide slide-02">
         </div>
-
-        <div class="lightbox__slide">
-            <img src="photography/img_0359-folio.jpg">
+        <div class="lightbox__slide slide-03">
         </div>
-
-        <div class="lightbox__slide">
-            <img src="photography/img_0387-folio.jpg">
+        <div class="lightbox__slide slide-04">
         </div>
-
-        <div class="lightbox__slide">
-            <img src="photography/img_4453-folio.jpg">
+        <div class="lightbox__slide slide-05">
         </div>
-
-        <div class="lightbox__slide">
-            <img src="photography/img_4480-folio.jpg">
+        <div class="lightbox__slide slide-06">
         </div>
-
-        <div class="lightbox__slide">
-            <img src="photography/img_3889-folio.jpg">
+        <div class="lightbox__slide slide-07">
         </div>
-
-        <div class="lightbox__slide">
-            <img src="photography/img_3251-folio.jpg">
+        <div class="lightbox__slide slide-08">
         </div>
-
         <a class="lightbox__viewer--prev" onclick="plusSlides(-1)"><i class="fa fa-angle-left" aria-hidden="true"></i></a>
         <a class="lightbox__viewer--next" onclick="plusSlides(1)"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
-
     </div>
-
 </div>
