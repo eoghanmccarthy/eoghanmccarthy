5a2c1a5bed405dbc150795a325515d4a6bf24900
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-01-24 09:50:20 +0000

    gulp

--- partials/scripts.php
+++ partials/scripts.php
@@ -11,5 +11,6 @@
 <script src="https://use.fontawesome.com/e6985d2f93.js"></script>
 
 <!-- Custom -->
-<link href="css/main.css" rel="stylesheet" type="text/css" />
-<script src="js/main.js"></script>
+<script src="src/js/dist/lightbox.js"></script>
+<link href="src/css/main.css" rel="stylesheet" type="text/css" />
+<script src="src/js/dist/main.js"></script>
