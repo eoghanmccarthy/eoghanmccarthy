e55d9f62192f2c0907d8040e46234c00ba10b575
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-02-01 21:07:43 +0000

    javascript

--- partials/header.php
+++ partials/header.php
@@ -3,10 +3,13 @@
         <a href="index.php"><?php include 'partials/logo.php';?></a>
     </div>
     <nav class="header__nav">
-        <ul class="nav-bar" id="topNav">
-            <li class="nav-bar__icon"><a href="javascript:void(0);" onclick="toggleNav()">&#9776;</a></li>
+        <ul class="nav-bar" id="navBar">
+            <li id="navBarIcon">&#9776;</li>
             <li><a href="photography.php">photography</a></li>
             <li><a href="contact.php">contact</a></li>
         </ul>
     </nav>
 </header>
+
+
+<!-- <li class="nav-bar__icon"><a href="javascript:void(0);" onclick="toggleNav()">&#9776;</a></li> -->
--- src/js/dist/main.js
+++ src/js/dist/main.js
@@ -5,11 +5,22 @@
  */
 
 /* Function to add mobile class when icon is clicked */
-function toggleNav() {
-    var x = document.getElementById("topNav");
-    if (x.className === "nav-bar") {
-        x.className += " nav-bar--xs";
+var navBar = document.getElementById("navBar");
+var navBarIcon = document.getElementById("navBarIcon");
+
+navBarIcon.addEventListener("click", function () {
+    if (navBar.className === "nav-bar") {
+        navBar.className += " nav-bar--xs";
     } else {
-        x.className = "nav-bar";
+        navBar.className = "nav-bar";
     }
-}
+});
+
+// function toggleNav() {
+//     let x = document.getElementById("topNav");
+//     if (x.className === "nav-bar") {
+//         x.className += " nav-bar--xs";
+//     } else {
+//         x.className = "nav-bar";
+//     }
+// }
