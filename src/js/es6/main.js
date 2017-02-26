615412b125b8329af3e507e8c8ef9827fb6d9853
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-02-26 16:14:15 +0000

    es5

--- src/js/es6/main.js
+++ src/js/es6/main.js
@@ -2,42 +2,42 @@
  * Main
  */
 
-(() => {
-
-    // Web app
-    const Web = {
-        // Init
-        init: () => {
-            Web.nav.bind();
-        },
-        // Menu
-        nav: {
-            // Bind menu
-            bind: () => {
-                $('#navBarIcon').on('click', () => {
-                    let menu = $('#navBar');
-                    if (menu.className === 'nav-bar') {
-                        menu.className += ' nav-bar--xs';
-                    } else {
-                        menu.className = 'nav-bar';
-                    }
-                });
-            }
-        }
-    };
-
-    Web.init();
-
-})();
-
-// /* Function to add mobile class when icon is clicked */
-// const navBar = document.getElementById('navBar');
-// const navBarIcon = document.getElementById('navBarIcon');
+// (() => {
 //
-// navBarIcon.addEventListener("click", function() {
-//     if (navBar.className === 'nav-bar') {
-//         navBar.className += ' nav-bar--xs';
-//     } else {
-//         navBar.className = 'nav-bar';
-//     }
-// });
+//     // Web app
+//     const Web = {
+//         // Init
+//         init: () => {
+//             Web.nav.bind();
+//         },
+//         // Menu
+//         nav: {
+//             // Bind menu
+//             bind: () => {
+//                 $('#navBarIcon').on('click', () => {
+//                     let menu = $('#navBar');
+//                     if (menu.className === 'nav-bar') {
+//                         menu.className += ' nav-bar--xs';
+//                     } else {
+//                         menu.className = 'nav-bar';
+//                     }
+//                 });
+//             }
+//         }
+//     };
+//
+//     Web.init();
+//
+// })();
+
+/* Function to add mobile class when icon is clicked */
+const navBar = document.getElementById('navBar');
+const navBarIcon = document.getElementById('navBarIcon');
+
+navBarIcon.addEventListener("click", function() {
+    if (navBar.className === 'nav-bar') {
+        navBar.className += ' nav-bar--xs';
+    } else {
+        navBar.className = 'nav-bar';
+    }
+});
