fb2aa86ed28281857134e8c91bf2a835ca6868d6
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-03-13 22:31:31 +0000

    es6

--- src/js/dist/main.js
+++ src/js/dist/main.js
@@ -4,45 +4,46 @@
  * Main
  */
 
-(function () {
-
-    // Web app
-    var Web = {
-        // Init
-        init: function init() {
-            Web.bind();
-        },
-        // Bind
-        bind: function bind() {
-            Web.menu.bind();
-        },
-        // Menu
-        menu: {
-            // Bind
-            bind: function bind() {
-                $('#menu-toggle').on('click', function () {
-                    var m = $('#menu');
-                    if (m.className === 'menu-nav__list') {
-                        m.className += ' menu-nav__list--xs';
-                    } else {
-                        m.className = 'menu-nav__list';
-                    }
-                });
-            }
-        }
-    };
-
-    Web.init();
-})();
-
-// /* Function to add mobile class when icon is clicked */
-// const navBar = document.getElementById('menu');
-// const navBarIcon = document.getElementById('menu-toggle');
+// (() => {
 //
-// navBarIcon.addEventListener("click", function() {
-//     if (navBar.className === 'menu-nav__list') {
-//         navBar.className += ' menu-nav__list--xs';
-//     } else {
-//         navBar.className = 'menu-nav__list';
-//     }
-// });
+//     // Web app
+//     var Web = {
+//         // Init
+//         init: () => {
+//             Web.bind();
+//         },
+//         // Bind
+//         bind: () => {
+//         Web.menu.bind();
+//         },
+//         // Menu
+//         menu: {
+//             // Bind
+//             bind: () => {
+//                 $('#menu-toggle').on('click', () => {
+//                     let m = $('#menu');
+//                     if (m.className === 'menu-nav__list') {
+//                         m.className += ' menu-nav__list--xs';
+//                     } else {
+//                         m.className = 'menu-nav__list';
+//                     }
+//                 });
+//             }
+//         }
+//     };
+//
+//     Web.init();
+//
+// })();
+
+// Function to add mobile class when icon is clicked
+var menu = $('#menu');
+var menuToggle = $('#menu-toggle');
+
+menuToggle.on('click', function () {
+	if (menu.className === 'menu-nav__list') {
+		menu.className += ' menu-nav__list--xs';
+	} else {
+		menu.className = 'menu-nav__list';
+	}
+});
--- src/js/es6/main.js
+++ src/js/es6/main.js
@@ -2,46 +2,46 @@
  * Main
  */
 
-(() => {
-
-    // Web app
-    var Web = {
-        // Init
-        init: () => {
-            Web.bind();
-        },
-        // Bind
-        bind: () => {
-        Web.menu.bind();
-        },
-        // Menu
-        menu: {
-            // Bind
-            bind: () => {
-                $('#menu-toggle').on('click', () => {
-                    let m = $('#menu');
-                    if (m.className === 'menu-nav__list') {
-                        m.className += ' menu-nav__list--xs';
-                    } else {
-                        m.className = 'menu-nav__list';
-                    }
-                });
-            }
-        }
-    };
-
-    Web.init();
-
-})();
-
-// /* Function to add mobile class when icon is clicked */
-// const navBar = document.getElementById('menu');
-// const navBarIcon = document.getElementById('menu-toggle');
+// (() => {
 //
-// navBarIcon.addEventListener("click", function() {
-//     if (navBar.className === 'menu-nav__list') {
-//         navBar.className += ' menu-nav__list--xs';
-//     } else {
-//         navBar.className = 'menu-nav__list';
-//     }
-// });
+//     // Web app
+//     var Web = {
+//         // Init
+//         init: () => {
+//             Web.bind();
+//         },
+//         // Bind
+//         bind: () => {
+//         Web.menu.bind();
+//         },
+//         // Menu
+//         menu: {
+//             // Bind
+//             bind: () => {
+//                 $('#menu-toggle').on('click', () => {
+//                     let m = $('#menu');
+//                     if (m.className === 'menu-nav__list') {
+//                         m.className += ' menu-nav__list--xs';
+//                     } else {
+//                         m.className = 'menu-nav__list';
+//                     }
+//                 });
+//             }
+//         }
+//     };
+//
+//     Web.init();
+//
+// })();
+
+// Function to add mobile class when icon is clicked
+const menu = $('#menu');
+const menuToggle = $('#menu-toggle');
+
+menuToggle.on('click', () => {
+	if (menu.className === 'menu-nav__list') {
+		menu.className += ' menu-nav__list--xs';
+	} else {
+		menu.className = 'menu-nav__list';
+	}
+});
