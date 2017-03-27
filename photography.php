4ad951b4b1694d3c929e89a330793aa0537057eb
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-03-28 00:04:57 +0100

    php

--- photography.php
+++ photography.php
@@ -1,79 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
+	<head>
 
-        <title>eoghanmccarthy / photography</title>
+		<title>eoghanmccarthy / photography</title>
 
-        <?php include 'partials/meta.php';?>
-        <?php include 'partials/scripts.php';?>
+		<?php include 'partials/meta.php';?>
+		<?php include 'partials/scripts.php';?>
 
-    </head>
+	</head>
 
-    <body>
+	<body>
 
-        <!-- Container -->
-        <div class="container">
+		<!-- Container -->
+		<div class="container">
 
-            <!-- Header -->
-            <?php include 'partials/header.php';?>
+			<!-- Header -->
+			<?php include 'partials/header.php';?>
 
-            <!-- Main -->
-            <main>
-                <div id="gallery" class="row">
-                    <div class="col-md-6">
+			<!-- Main -->
+			<main>
+				<div id="gallery" class="row">
+					<div class="col-md-6">
+						<figure class="media">
+							<img class="media__content" src="photography/img_0111-folio.jpg" title="abashiri, japan">
+							<figcaption class="media__meta">abashiri, japan</figcaption>
+						</figure>
+					</div>
+					<div class="col-md-6">
+						<figure class="media">
+							<img class="media__content" src="photography/img_0243-folio.jpg" title="sapporo, japan">
+							<figcaption class="media__meta">sapporo, japan</figcaption>
+						</figure>
+					</div>
+					<div class="col-md-6">
+						<figure class="media">
+							<img class="media__content" src="photography/img_0359-folio.jpg" title="abashiri, japan">
+							<figcaption class="media__meta">abashiri, japan</figcaption>
+						</figure>
+					</div>
+					<div class="col-md-6">
                         <figure class="media">
-                            <img class="media__content" src="photography/img_0111-folio.jpg">
+                            <img class="media__content" src="photography/img_0387-folio.jpg" title="abashiri, japan">
                             <figcaption class="media__meta">abashiri, japan</figcaption>
                         </figure>
                     </div>
                     <div class="col-md-6">
                         <figure class="media">
-                            <img class="media__content" src="photography/img_0243-folio.jpg">
-                            <figcaption class="media__meta">sapporo, japan</figcaption>
-                        </figure>
-                    </div>
-                    <div class="col-md-6">
-                        <figure class="media">
-                            <img class="media__content" src="photography/img_0359-folio.jpg">
-                            <figcaption class="media__meta">abashiri, japan</figcaption>
-                        </figure>
-                    </div>
-                    <div class="col-md-6">
-                        <figure class="media">
-                            <img class="media__content" src="photography/img_0387-folio.jpg">
-                            <figcaption class="media__meta">abashiri, japan</figcaption>
-                        </figure>
-                    </div>
-                    <div class="col-md-6">
-                        <figure class="media">
-                            <img class="media__content" src="photography/img_4453-folio.jpg">
+                            <img class="media__content" src="photography/img_4453-folio.jpg" title="shizuoka, japan">
                             <figcaption class="media__meta">shizuoka, japan</figcaption>
                         </figure>
                     </div>
                     <div class="col-md-6">
                         <figure class="media">
-                            <img class="media__content" src="photography/img_4480-folio.jpg">
+                            <img class="media__content" src="photography/img_4480-folio.jpg" title="shizuoka, japan">
                             <figcaption class="media__meta">shizuoka, japan</figcaption>
                         </figure>
                     </div>
                     <div class="col-md-6">
                         <figure class="media">
-                            <img class="media__content" src="photography/img_3889-folio.jpg">
+                            <img class="media__content" src="photography/img_3889-folio.jpg" title="tokyo, japan">
                             <figcaption class="media__meta">tokyo, japan</figcaption>
                         </figure>
                     </div>
                     <div class="col-md-6">
                         <figure class="media">
-                            <img class="media__content" src="photography/img_3251-folio.jpg">
+                            <img class="media__content" src="photography/img_3251-folio.jpg" title="izu, japan">
                             <figcaption class="media__meta">izu, japan</figcaption>
                         </figure>
                     </div>
                 </div>
             </main>
 
-            <!-- Footer -->
-            <?php include 'partials/footer.php';?>
+			<!-- Footer -->
+			<?php include 'partials/footer.php';?>
 
 		</div>
 
