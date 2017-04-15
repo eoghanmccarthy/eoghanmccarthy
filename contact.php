2c6580e5e93b0d7d19cd2c85fd6b7e3f5745f00e
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-04-15 23:20:05 +0100

    fix

--- contact.php
+++ contact.php
@@ -1,39 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
+	<head>
+		<title>eoghanmccarthy / contact</title>
+		<?php include 'partials/meta.php';?>
+		<?php include 'partials/scripts.php';?>
+	</head>
 
-        <title>eoghanmccarthy / contact</title>
-
-        <?php include 'partials/meta.php';?>
-        <?php include 'partials/scripts.php';?>
-
-    </head>
-
-    <body id="contact">
-
-        <!-- Container -->
-
-        <div class="container">
-
-            <!-- Header -->
-
-            <?php include 'partials/header.php';?>
-
-            <!-- Main -->
-
-            <main>
-                <p><mark class="mark mark--dev">mail@eoghanmccarthy.com</mark><br><mark class="mark mark--dev">+44 (0)7490 816 795</mark></p>
-            </main>
-
-            <!-- Footer -->
-
-            <?php include 'partials/footer.php';?>
-
-        </div>
-
-        <?php include 'partials/scripts-js.php';?>
-
-    </body>
+	<body id="contact">
+		<div class="container">
+			<!-- Header -->
+			<?php include 'partials/header.php';?>
+			<!-- Main -->
+			<main>
+				<p><mark class="mark mark--dev">mail@eoghanmccarthy.com</mark><br><mark class="mark mark--dev">+44 (0)7490 816 795</mark></p>
+			</main>
+			<!-- Footer -->
+			<?php include 'partials/footer.php';?>
+		</div>
+		<?php include 'partials/scripts-js.php';?>
+	</body>
 
 </html>
