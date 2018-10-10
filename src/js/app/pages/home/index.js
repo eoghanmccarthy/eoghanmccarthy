f43aa2bf94d83ff9b051a9188f6ab31f7ad16c87
Eoghan Mccarthy <eoghanmccarthy@outlook.com>
2018-10-10 23:34:48 +0100

    home page

---
+++ src/js/app/pages/home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import Header from "containers/header";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className={"index"}>
+        <Header />
+      </div>
+    );
+  }
+}
