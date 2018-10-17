113cab9993d5fd61457cc803a8dc369ba5c3e649
Eoghan Mccarthy <eoghanmccarthy@outlook.com>
2018-10-18 00:32:21 +0100

    run build

--- src/js/app/config/galleries/japan.js
+++ src/js/app/config/galleries/japan.js
@@ -1,43 +1,43 @@
-const IMG_1 = require("assets/images/img_0359-folio.jpg");
-const IMG_2 = require("assets/images/img_0243-folio.jpg");
-const IMG_3 = require("assets/images/img_0111-folio.jpg");
-const IMG_4 = require("assets/images/img_0387-folio.jpg");
-const IMG_5 = require("assets/images/img_4453-folio.jpg");
-const IMG_6 = require("assets/images/img_4480-folio.jpg");
-const IMG_7 = require("assets/images/img_3889-folio.jpg");
-const IMG_8 = require("assets/images/img_3251-folio.jpg");
+const IMG_01 = require("assets/images/img_0359-folio.jpg");
+const IMG_02 = require("assets/images/img_0243-folio.jpg");
+const IMG_03 = require("assets/images/img_0111-folio.jpg");
+const IMG_04 = require("assets/images/img_0387-folio.jpg");
+const IMG_05 = require("assets/images/img_4453-folio.jpg");
+const IMG_06 = require("assets/images/img_4480-folio.jpg");
+const IMG_07 = require("assets/images/img_3889-folio.jpg");
+const IMG_08 = require("assets/images/img_3251-folio.jpg");
 
 const data = [
   {
-    src: IMG_1,
+    src: IMG_01,
     label: "abashiri, japan"
   },
   {
-    src: IMG_2,
+    src: IMG_02,
     label: "sapporo, japan"
   },
   {
-    src: IMG_3,
+    src: IMG_03,
     label: "abashiri, japan"
   },
   {
-    src: IMG_4,
+    src: IMG_04,
     label: "abashiri, japan"
   },
   {
-    src: IMG_5,
+    src: IMG_05,
     label: "shizuoka, japan"
   },
   {
-    src: IMG_6,
+    src: IMG_06,
     label: "shizuoka, japan"
   },
   {
-    src: IMG_7,
+    src: IMG_07,
     label: "tokyo, japan"
   },
   {
-    src: IMG_8,
+    src: IMG_08,
     label: "izu, japan"
   }
 ];
