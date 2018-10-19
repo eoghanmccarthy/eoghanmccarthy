8ff19ca476d88a1a860f93ce1bfc97631682ac0f
Eoghan Mccarthy <eoghanmccarthy@outlook.com>
2018-10-19 20:58:38 +0100

    run build

--- src/js/app/components/lightbox/slides/index.js
+++ src/js/app/components/lightbox/slides/index.js
@@ -4,18 +4,14 @@ export default class Slides extends Component {
   render() {
     const { data, slideIndex } = this.props;
 
-    return (
-      <div>
-        {data.map((item, index) => (
-          <div
-            key={index}
-            className={index !== slideIndex ? "slide" : "slide active"}
-            style={{ backgroundImage: `url(${item.src})` }}
-          >
-            <span className="slide__caption">{item.label}</span>
-          </div>
-        ))}
+    return data.map((item, index) => (
+      <div
+        key={index}
+        className={index !== slideIndex ? "slide" : "slide active"}
+        style={{ backgroundImage: `url(${item.src})` }}
+      >
+        <span className="slide__caption">{item.label}</span>
       </div>
-    );
+    ));
   }
 }
