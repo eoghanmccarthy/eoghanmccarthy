6d8480d7407912b3b6b5b900f671924254f801c9
Eoghan Mccarthy <eoghanmccarthy@outlook.com>
2019-01-04 18:30:50 +0000

    header styling

--- src/js/app/components/header/index.tsx
+++ src/js/app/components/header/index.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 
 import Logo from "components/logo";
-import Navigation from "components/globalNavigation";
+import GlobalNavigation from "components/globalNavigation";
 
 const Header: React.FunctionComponent<{}> = () => (
   <header className={"me__header"}>
-    <div className={"header__logo"}>
-      <Logo />
-    </div>
-    <div className={"header__nav"}>
-      <Navigation />
-    </div>
+    <Logo />
+    <GlobalNavigation />
   </header>
 );
 
