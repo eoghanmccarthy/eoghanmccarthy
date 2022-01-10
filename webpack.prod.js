0b238417037917c736ae78ef2fdd9ea3c9b0af44
Eoghan McCarthy <eoghanmccarthy@outlook.com>
2022-01-10 22:13:43 +0000

    build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,39 +1,40 @@
-const webpack = require('webpack');
-const { merge } = require('webpack-merge');
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
   output: {
-    filename: '[name].[contenthash].bundle.js',
-    chunkFilename: '[id].js'
+    filename: "[name].[contenthash].bundle.js",
+    chunkFilename: "[id].js",
+    clean: true,
   },
   optimization: {
     minimize: true,
-    minimizer: ['...', new CssMinimizerPlugin()],
-    moduleIds: 'deterministic',
-    runtimeChunk: 'single',
+    minimizer: ["...", new CssMinimizerPlugin()],
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
     splitChunks: {
       //https://webpack.js.org/plugins/split-chunks-plugin/#split-chunks-example-2
       //https://webpack.js.org/plugins/split-chunks-plugin/#split-chunks-example-3
-      chunks: 'all',
+      chunks: "all",
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
-    }
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
   },
   plugins: [
-    new CopyWebpackPlugin({ patterns: [{from: "./app/public" }]}),
+    new CopyWebpackPlugin({ patterns: [{ from: "./app/public" }] }),
     new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css'
-    })
-  ]
+      filename: "[name].[contenthash].css",
+    }),
+  ],
 });
