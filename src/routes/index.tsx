270c136fc681b19e6940f08123f17bdd1fab41c7
Eoghan McCarthy <eoghanmccarthy@outlook.com>
2025-12-31 22:03:36 +0900

    Layout styling

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,5 +3,5 @@ import { createFileRoute } from "@tanstack/react-router";
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
-  return <div>Eoghan McCarthy</div>;
+  return <div className="py-32">Eoghan McCarthy</div>;
 }
