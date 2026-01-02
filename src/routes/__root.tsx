74aba9bfbc07ff115973b452d917b0dee8b60c8d
Eoghan McCarthy <eoghanmccarthy@outlook.com>
2026-01-02 20:52:30 +0900

    Run format

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -37,7 +37,8 @@ function RootDocument({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <style>
-          @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+          @import
+          url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
         </style>
         <HeadContent />
       </head>
